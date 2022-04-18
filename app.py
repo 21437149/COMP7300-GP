dca2f1affab1200887309448ce6a6a1cdb248774
LAO ZhiCong <winson.lo@life.hkbu.edu.hk>
2022-04-18 09:53:22 +0800

    替换github oauth key

--- app.py
+++ app.py
@@ -8,17 +8,20 @@ finnhub_client = finnhub.Client(api_key="c9cdsciad3i8nttpf2pg")
 
 # app = Flask(__name__)
 app = Flask(__name__,
-            static_url_path='', 
+            static_url_path='',
             static_folder='assets',
             template_folder='views')
 CORS(app)
 app.secret_key = "please_define_a_secret_key_here"
-app.config["GITHUB_OAUTH_CLIENT_ID"] = 'eac3bb989c74957c1c7a'
-app.config["GITHUB_OAUTH_CLIENT_SECRET"] = 'b41d1c91bc657ab3db0dc5d65d089be102a73d46'
+# app.config["GITHUB_OAUTH_CLIENT_ID"] = 'eac3bb989c74957c1c7a'
+# app.config["GITHUB_OAUTH_CLIENT_SECRET"] = 'b41d1c91bc657ab3db0dc5d65d089be102a73d46'
+app.config["GITHUB_OAUTH_CLIENT_ID"] = 'fe75b86dc0f4e11eb2e0'
+app.config["GITHUB_OAUTH_CLIENT_SECRET"] = '849f52fb3dff1bf71ad1b7070e99544d6af16d7a'
 blueprint = make_github_blueprint()
 app.register_blueprint(make_github_blueprint(), url_prefix="/login")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./test.db'
 db = SQLAlchemy(app)
+
 
 class UserStock(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -26,7 +29,8 @@ class UserStock(db.Model):
     symbol = db.Column(db.String(50), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<UserStock %r>' % self.username + ' ' + self.symbol  
+        return '<UserStock %r>' % self.username + ' ' + self.symbol
+
 
 db.drop_all()
 db.create_all()
@@ -34,14 +38,16 @@ db.create_all()
 # def hello_world():
 #     return "<p>Hello, World!</p>"
 
+
 @app.route("/")
 def index():
     general_news = finnhub_client.general_news('general', min_id=0)
     if github.authorized:
         github_user = github.get("/user").json()
-        return render_template( 'index.html', title = ' - Index', login = github_user['login'], general_news = general_news)
+        return render_template('index.html', title=' - Index', login=github_user['login'], general_news=general_news)
     else:
-        return render_template('index.html', title = ' - Index', general_news = general_news)
+        return render_template('index.html', title=' - Index', general_news=general_news)
+
 
 @app.route("/login")
 def login():
@@ -49,7 +55,8 @@ def login():
         return redirect(url_for("github.login"))
     return redirect('/')
 
-@app.route("/portfolio", methods = ['GET', 'POST'])
+
+@app.route("/portfolio", methods=['GET', 'POST'])
 def portfolio():
     if not github.authorized:
         return redirect('/login')
@@ -59,7 +66,8 @@ def portfolio():
         db.session.add(UserStock(username=github_user['login'], symbol=symbol))
         db.session.commit()
     stocks = UserStock.query.filter_by(username=github_user['login'])
-    return render_template('portfolio.html', login = github_user['login'], title = ' - Portfolio', stocks = stocks)
+    return render_template('portfolio.html', login=github_user['login'], title=' - Portfolio', stocks=stocks)
+
 
 @app.route("/portfolio/<symbol>", methods=['DELETE'])
 def deletePortfolio(symbol):
@@ -67,9 +75,9 @@ def deletePortfolio(symbol):
         return redirect('/login')
 
     github_user = github.get("/user").json()
-    stocks = UserStock.query.filter_by(username=github_user['login'], symbol=symbol)
+    stocks = UserStock.query.filter_by(
+        username=github_user['login'], symbol=symbol)
     for s in stocks:
         db.session.delete(s)
     db.session.commit()
     return 'ok'
-
